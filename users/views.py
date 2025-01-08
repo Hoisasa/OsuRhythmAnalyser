a21f1770bfe63848f12c4581dbbadbe20f0b0d6a
Raffie <ob4zou@gmail.com>
2025-01-08 11:44:06 +0200

    Added missing project files

--- users/views.py
+++ users/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login, logout
+from django import forms
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your username'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password'})
+    )
+
+
+class CustomUserCreateForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your username'})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password'})
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirm your password'})
+    )
 
 
 def register_view(request):
@@ -18,8 +40,8 @@ def register_view(request):
             if form.is_valid():
                 form.save()
                 return redirect("DjangoService:MusicCollection")
-    context = {'formRegstr': UserCreationForm(),
-               'formLogin': AuthenticationForm()}
+    context = {'formRegstr': CustomUserCreateForm(),
+               'formLogin': CustomAuthenticationForm()}
     return render(request, 'registration/login.html', context)
 
 
